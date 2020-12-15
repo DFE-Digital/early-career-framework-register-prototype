d68eddd4b2d5355145e56eb6429a60f6738b7e64
Alaric King <alaric@alaricking.co.uk>
2020-12-15 15:46:12 +0000

    sign in and create account pages and branch

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,17 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+
+// Branching for Prove identity
+router.post('/identity-answer', function (req, res) {
+  const identityAnswer = req.session.data['identity-answer']
+  if (identityAnswer === 'sign-in') {
+    res.redirect('/sign-in')
+  } else {
+    res.redirect('/create-account-1')
+  }
+})
+
+
+
 module.exports = router
