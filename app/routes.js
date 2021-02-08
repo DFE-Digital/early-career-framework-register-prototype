83be3dd3b6bb28e775161916c1e6a928928880c1
Alaric King <alaric@alaricking.co.uk>
2021-02-08 16:09:32 +0000

    refactor for Register and Partner

--- app/routes.js
+++ app/routes.js
@@ -4,13 +4,23 @@ const router = express.Router()
 // Add your routes here - above the module.exports line
 
 
-// Branching for Prove identity
-router.post('/identity-answer', function (req, res) {
-  const identityAnswer = req.session.data['identity-answer']
-  if (identityAnswer === 'sign-in') {
-    res.redirect('/sign-in')
+// Branching for register-school-2
+router.post('/register-school/register-school-3', function (req, res) {
+  const willYouBeTrainingAnswer = req.session.data['will-you-be-training']
+  if (willYouBeTrainingAnswer === 'yes') {
+    res.redirect('/register-school/register-school-3')
   } else {
-    res.redirect('/create-account-1')
+    res.redirect('/register-school/register-later-no-participants')
+  }
+})
+
+// Branching for register-school-3
+router.post('/register-school/register-school-4', function (req, res) {
+  const chosenInductionTraining = req.session.data['chosen-induction-training']
+  if (chosenInductionTraining === 'yes') {
+    res.redirect('/register-school/register-school-4')
+  } else {
+    res.redirect('/register-school/register-later-no-decision')
   }
 })
 
