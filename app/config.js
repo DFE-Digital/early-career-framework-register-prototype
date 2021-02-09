bace0564aaa6135470b17e0b6bbff78b2c7be065
Alaric King <alaric@alaricking.co.uk>
2021-02-09 18:20:00 +0000

    updating service name

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Training for new teachers',
+  serviceName: 'Induction for early career teachers',
 
   // Default port that prototype runs on
   port: '3000',
