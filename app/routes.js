e9357d226cfdc26eee1ebad1e6920587efe69069
Alaric King <alaric@alaricking.co.uk>
2021-02-16 18:29:28 +0000

    TP routing and dashboard

--- app/routes.js
+++ app/routes.js
@@ -4,6 +4,34 @@ const router = express.Router()
 // Add your routes here - above the module.exports line
 
 
+// Branching for sign in
+router.post('/sign-in-check-email', function (req, res) {
+  const journeyMode = req.session.data['journey-mode']
+  if (journeyMode === 'noaccount') {
+    res.redirect('/sign-in-no-account')
+  }
+  // disabling school sign in for the timebeing
+  else if (journeyMode === 'schooldecisionmaker') {
+    res.redirect('/sign-in-no-account')
+  } else {
+    res.redirect('/sign-in-check-email')
+  }
+})
+
+// Branching for access to dashboard (/sign-in-continue-to-account)
+router.post('/dashboard', function (req, res) {
+  const journeyMode = req.session.data['journey-mode']
+  if (journeyMode === 'trainingprovider') {
+    res.redirect('/training-provider-signed-in/dashboard-training-provider')
+  }
+  // disabling school sign in for the timebeing
+  else if (journeyMode === 'schooldecisionmaker') {
+    res.redirect('/sign-in-no-account')
+  } else {
+    res.redirect('/sign-in-no-account')
+  }
+})
+
 // Branching for register-school-2
 router.post('/register-school/register-school-3', function (req, res) {
   const willYouBeTrainingAnswer = req.session.data['will-you-be-training']
