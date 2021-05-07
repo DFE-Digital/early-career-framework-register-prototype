f14d9b9bdb4f19e9105bffd475c238a2045b4771
Alaric King <alaric@alaricking.co.uk>
2021-05-07 09:01:33 +0100

    Footer update and choose provision journey

--- app/routes.js
+++ app/routes.js
@@ -51,7 +51,7 @@ router.post('/dashboard', function (req, res) {
 })
 
 // Branching on choosing provision
-router.post('/school-signed-in/no-decision/prompt-choose-provision', function (req, res) {
+router.post('/school-signed-in/no-decision/provision-confirmed', function (req, res) {
   const journeyMode = req.session.data['journey-mode']
   if (journeyMode === 'schoolFIP') {
     res.redirect('/school-signed-in/fip/fip-cohort-detail')
