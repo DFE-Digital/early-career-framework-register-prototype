f430964e66704c68f54ab76748c1cc2c464bf1b3
Paul Hayes <fofr@users.noreply.github.com>
2021-05-27 17:31:34 +0100

    Add name pages, qualified context, details and check answers

--- app/utils/teacher-or-mentor-validation-wizard-paths.js
+++ app/utils/teacher-or-mentor-validation-wizard-paths.js
@@ -6,8 +6,13 @@ const {
 function validationWizardPaths (req) {
   var paths = [
     '/teacher-or-mentor/validation/privacy-policy',
+    '/teacher-or-mentor/validation/qualified-check',
     '/teacher-or-mentor/validation/trn',
-    '/teacher-or-mentor/validation/name-changed'
+    '/teacher-or-mentor/validation/name-changed',
+    '/teacher-or-mentor/validation/name-updated',
+    '/teacher-or-mentor/validation/details',
+    '/teacher-or-mentor/validation/check',
+    '/teacher-or-mentor/validation/confirmation'
   ]
   return nextAndBackPaths(paths, req)
 }
