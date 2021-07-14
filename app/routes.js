2b34388589586401380eea517a806d6ccf6f7f7c
Brian Coult <brian.coult@digital.education.gov.uk>
2021-07-14 13:17:38 +0100

    broken links but also changed the preload to still go through the sign in as its part of the flow

--- app/routes.js
+++ app/routes.js
@@ -4,18 +4,18 @@ const router = express.Router()
 // Add your routes here - above the module.exports line
 
 // Branching for sign in
-router.post('/admin/preload', function (req, res) {
-  const journeyMode = req.session.data['journey-mode']
-  if (journeyMode === 'schoolNoDecision') {
-    res.redirect('/sprint-16/school-nominate-school-lead/nominate-school-lead-1')
-  } else if (journeyMode === 'schoolFIP') {
-    res.redirect('/sprint-16/school-signed-in/fip/fip-choose-cohort')
-  } else if (journeyMode === 'schoolCIP') {
-    res.redirect('/sprint-16/school-signed-in/cip/cip-choose-cohort')
-  } else {
-    res.redirect('/sprint-16/start')
-  }
-})
+// router.post('/admin/preload', function (req, res) {
+//   const journeyMode = req.session.data['journey-mode']
+//   if (journeyMode === 'schoolNoDecision') {
+//     res.redirect('/sprint-16/school-nominate-school-lead/nominate-school-lead-1')
+//   } else if (journeyMode === 'schoolFIP') {
+//     res.redirect('/sprint-16/school-signed-in/fip/fip-choose-cohort')
+//   } else if (journeyMode === 'schoolCIP') {
+//     res.redirect('/sprint-16/school-signed-in/cip/cip-choose-cohort')
+//   } else {
+//     res.redirect('/sprint-16/start')
+//   }
+// })
 
 
 
