c940068465ffa04caa23cd3fb747ab53ff4b3ed7
Paul Hayes <fofr@users.noreply.github.com>
2021-11-26 12:33:05 +0000

    Allow name and school to be set with env vars

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -1,29 +1,18 @@
-/*
+const defaultSchoolName = 'Acme Primary School'
+const defaultUserFullName = 'Hugo Furst'
 
-Provide default values for user session data. These are automatically added
-via the `autoStoreData` middleware. Values will only be added to the
-session if a value doesn't already exist. This may be useful for testing
-journeys where users are returning or logging in to an existing application.
-
-============================================================================
-
-Example usage:
-
-"full-name": "Sarah Philips",
-
-"options-chosen": [ "foo", "bar" ]
-
-============================================================================
-
-*/
+const schoolName = process.env.SCHOOL_NAME || defaultSchoolName
+const userFullName = process.env.FULL_NAME || defaultUserFullName
+const userFirstName = userFullName.split(' ')[0]
+const userEmailAddress = `${userFullName.toLowerCase().replace(' ', '.')}@dfe.test.gov.uk`
 
 module.exports = {
-  schoolName: 'Acme Primary School',
-  userFullName: 'Hugo Furst',
-  userFirstName: 'Hugo',
-  leadProvider: 'Ambition Institute',
-  deliveryPartner: 'Mega Delivery Partner',
-  userEmailAddress: 'hugo.furst@dfe.test.gov.uk',
+  schoolName,
+  userFullName,
+  userFirstName,
+  userEmailAddress,
+  leadProvider: '',
+  deliveryPartner: '',
   linkExpire: '12 December 2021',
   expiryTime: '1pm'
 }
