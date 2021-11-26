4525a04df547b3f479b31075b52303458fea849e
Paul Hayes <fofr@users.noreply.github.com>
2021-11-26 12:44:15 +0000

    Allow setting of lead provider and delivery partner

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -1,18 +1,24 @@
-const defaultSchoolName = 'Acme Primary School'
-const defaultUserFullName = 'Hugo Furst'
+const defaults = {
+  schoolName: 'Acme Primary School',
+  userFullName: 'Hugo Furst',
+  leadProvider: 'Ambition Institute',
+  deliveryPartner: 'Mega Delivery Partner'
+}
 
-const schoolName = process.env.SCHOOL_NAME || defaultSchoolName
-const userFullName = process.env.FULL_NAME || defaultUserFullName
+const schoolName = process.env.SCHOOL_NAME || defaults.schoolName
+const userFullName = process.env.FULL_NAME || defaults.userFullName
 const userFirstName = userFullName.split(' ')[0]
 const userEmailAddress = `${userFullName.toLowerCase().replace(' ', '.')}@dfe.test.gov.uk`
+const deliveryPartner = process.env.DELIVERY_PARTNER || defaults.deliveryPartner
+const leadProvider = process.env.LEAD_PROVIDER || defaults.leadProvider
 
 module.exports = {
   schoolName,
   userFullName,
   userFirstName,
   userEmailAddress,
-  leadProvider: '',
-  deliveryPartner: '',
+  leadProvider,
+  deliveryPartner,
   linkExpire: '12 December 2021',
   expiryTime: '1pm'
 }
