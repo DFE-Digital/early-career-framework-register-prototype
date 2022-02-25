8cf1533ea11172802f6108d75f967bf0e14b7f0c
Brian Coult <brian.coult@digital.education.gov.uk>
2022-02-25 15:46:25 +0000

    fixed id for change of lead provider

--- app/routes/schools.js
+++ app/routes/schools.js
@@ -1,56 +1,55 @@
 const { generateRandomString } = require('../utils/helpers')
 
 module.exports = router => {
-  router.all([
-    '/support/schools/:id',
-    '/support/schools/:id/*'
-  ], (req, res, next) => {
-    const data = req.session.data
-    res.locals.id = req.params.id
-    res.locals.school = data.schools && data.schools[req.params.id]
-    next()
-  })
+    router.all([
+        '/support/schools/:id',
+        '/support/schools/:id/*'
+        ], (req, res, next) => {
+            const data = req.session.data
+            res.locals.id = req.params.id
+            res.locals.school = data.schools && data.schools[req.params.id]
+        next()
+    })
 
-  router.all('/support/schools', (req, res, next) => {
-    const participants = req.session.data.schools
-    // res.locals.hasECTs = Object.values(participants).some((p) => p.type === 'ect')
-    // res.locals.hasAnyContactedParticipants = Object.values(participants).some((p) => p.status === 'Contacted' && p.programme == "FIP"  )
-    // res.locals.hasAnyEligibleMentorParticipantsCIP = Object.values(participants).some((p) => p.status === 'Eligible' && p.type == "mentor" && p.programme == "CIP" )
-    next()
-  })
+    router.all('/support/schools', (req, res, next) => {
+        const school = req.session.data.schools
+        next()
+    })
 
-  router.all('/support/schools', (req, res, next) => {
-    const participants = req.session.data.schools
-    next()
-  })
+    // Schools overview
+    router.all('/support/schools/:id/overview', (req, res) => {
+        res.render('support/schools/overview')
+    })
+    router.all('/support/schools/:id/overview/:view', (req, res) => {
+        res.render(`support/schools/overview/${req.params.view}`)
+    })
 
-  router.all('/support/schools/:id/overview', (req, res) => {
-    res.render('support/schools/overview')
-  })
+    // Schools participants
+    router.all('/support/schools/:id/participants', (req, res) => {
+        res.render('support/schools/participants')
+    })
+    router.all('/support/schools/:id/participants/:view', (req, res) => {
+        res.render(`support/schools/participants/${req.params.view}`)
+    })
 
-  router.all('/support/schools/:id/participants', (req, res) => {
-    res.render('support/schools/participants')
-  })
+    // Schools cohorts
+    router.all('/support/schools/:id/cohorts', (req, res) => {
+        res.render('support/schools/cohorts')
+    })
+    router.all('/support/schools/:id/cohorts/:view', (req, res) => {
+        res.render(`support/schools/cohorts/${req.params.view}`)
+    })
 
-  router.all('/support/schools/:id/participants/:view', (req, res) => {
-    res.render(`support/schools/participants/${req.params.view}`)
-  })
 
-  router.all('/support/schools/:id/change-lead-provider', (req, res) => {
-    res.render('support/schools/change-lead-provider/why-are-you-changing')
-  })
+    // Lead provider change
+    router.all('/support/schools/:id/change-lead-provider', (req, res) => {
+        res.render('support/schools/change-lead-provider/why-are-you-changing')
+    })
+    router.all('/support/schools/:id/change-lead-provider/:view', (req, res) => {
+        res.render(`support/schools/change-lead-provider/${req.params.view}`)
+    })
 
-  router.all('/support/schools/:id/change-lead-provider/:view', (req, res) => {
-    res.render(`support/schools/change-lead-provider/${req.params.view}`)
-  })
 
-  router.all('/support/schools/:id/cohorts', (req, res) => {
-    res.render('support/schools/cohorts')
-  })
-
-  router.all('/support/schools/:id/cohorts/:view', (req, res) => {
-    res.render(`support/schools/cohorts/${req.params.view}`)
-  })
 
 
 }
