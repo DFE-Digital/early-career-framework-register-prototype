d9245ac1200b4e1e47159e2deb2a2bb6dfb554c2
Brian Coult <you@example.com>
2022-05-12 15:29:24 +0100

    mde multi cohort the default

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -26,9 +26,8 @@ module.exports = {
     inductionStart: 'Spring 2022',
     linkExpire: '2 March 2022',
     expiryTime: '1pm',
-    multi-cohort: 'Yes',
     inductionProgramme: "",
-    fipPartnership: "",
+    fipPartnership: "",    
     // rolesexplanation: "Seen",
     participants: {
         ME1: { cohort2021: "Yes", name: 'Jane Doe', email: 'jane.doe@example.com', type: 'mentor', programme: "CIP", status: 'Eligible', programme: "FIP", startDate: "Autumn 2021", usingCurrentTraining: "Yes" },
